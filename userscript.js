0b2e9b451a228054ca53ad443a1dab1822e6239c
Invertisment <visiskaj.slaptaj@gmail.com>
2019-07-05 17:18:38 +0300

    Fix using http sniffing

--- userscript.js
+++ userscript.js
@@ -9,63 +9,93 @@
 // @grant        none
 // ==/UserScript==
 
+// Notes: For best initialization use 'document-start' script placement in tampermonkey settings
+
 (function() {
-    'use strict';
+  'use strict';
 
-    function json(jsonString) {
-        return JSON.parse(jsonString);
-    }
+  function addXMLRequestCallback(callback) {
+    var oldSend, i;
+    if( XMLHttpRequest.callbacks ) {
+      // we've already overridden send() so just add the callback
+      XMLHttpRequest.callbacks.push( callback );
+    } else {
+      // create a callback queue
+      XMLHttpRequest.callbacks = [callback];
+      // store the native send()
+      oldSend = XMLHttpRequest.prototype.send;
+      // override the native send()
+      XMLHttpRequest.prototype.send = function() {
+        // call the native send()
+        oldSend.apply(this, arguments);
 
-    function getCursor() {
-        return json(localStorage.getItem("retrowave.player")).cursor;
-    }
-
-    function createHtmlElement() {
-        var element = document.createElement("div");
-        element.setAttribute("class", "display");
-        element.setAttribute("style", "display:flex;flex-direction:column;margin-top:55px;");
-        var parent = document.querySelector(".theme__container");
-        parent.appendChild(element);
-        return element;
-    }
-
-    function createTrackDlButton(track) {
-        var dwnldButton = document.createElement("a");
-        dwnldButton.innerHTML = track.title;
-        dwnldButton.setAttribute("download", track.title + ".mp3");
-        dwnldButton.setAttribute("href", track.streamUrl);
-        dwnldButton.setAttribute("class", "display__time__total");
-        dwnldButton.setAttribute("style", "align-self:center;width:auto;text-decoration:none;");
-        return dwnldButton;
-    }
-
-    function fillHtmlElement(parent, tracks) {
-        //console.log(parent, tracks);
-        parent.innerHTML = '';
-        tracks.forEach(track => parent.appendChild(createTrackDlButton(track)));
-    }
-
-    function updateCurrentSongs(cursor, htmlElement) {
-        var xmlHttp = new XMLHttpRequest();
-        xmlHttp.onreadystatechange = function() {
-            if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
-                fillHtmlElement(htmlElement, json(xmlHttp.responseText).body.tracks);
-            }
+        this.onreadystatechange = function ( progress ) {
+          for( i = 0; i < XMLHttpRequest.callbacks.length; i++ ) {
+            XMLHttpRequest.callbacks[i]( progress );
+          }
         };
-        xmlHttp.open("GET", window.location.protocol + "//retrowave.ru/api/v1/tracks" +
-                     "?cursor=" + cursor +
-                     "&limit=3",
-                     true);
-        xmlHttp.send(null);
+      }
     }
+  }
 
-    var buttonElement = createHtmlElement();
-    var updateFn = function() {
-        updateCurrentSongs(getCursor(), buttonElement);
-    };
-    window.setInterval(updateFn, 30000);
-    window.setTimeout(updateFn, 1000);
+  function json(jsonString) {
+    return JSON.parse(jsonString);
+  }
+
+  function createHtmlElement() {
+    var element = document.createElement("div");
+    element.setAttribute("class", "display dl-titles-container");
+    element.setAttribute("style", "display:flex;flex-direction:column;margin-top:55px;");
+    // :first-child { color: rebeccapurple }; :nth-child(2) { color: red }
+    var parent = document.querySelector(".theme__container");
+    parent.appendChild(element);
+    return element;
+  }
+
+  function createTrackDlButton(track) {
+    var dwnldButton = document.createElement("a");
+    dwnldButton.innerHTML = track.title;
+    dwnldButton.setAttribute("download", track.title + ".mp3");
+    dwnldButton.setAttribute("href", track.streamUrl);
+    dwnldButton.setAttribute("class", "display__time__total");
+    dwnldButton.setAttribute("style", "align-self:center;width:auto;text-decoration:none;");
+    return dwnldButton;
+  }
+
+  function fillHtmlElement(container, tracks) {
+    //console.log(container, tracks, window.songTitleContainerData.previouslyReceivedTracks == tracks);
+    if (JSON.stringify(tracks) == JSON.stringify(window.songTitleContainerData.previouslyReceivedTracks)) {
+      return;
+    }
+    window.songTitleContainerData.previouslyReceivedTracks = tracks;
+    tracks.forEach(track => container.prepend(createTrackDlButton(track)));
+    // Truncate to first three elements
+    while (container.childNodes.length > 3) {
+      container.removeChild(container.childNodes[3]);
+    }
+  }
+
+  function parseResponse(responseText, songContainer, trackListConsumerFn) {
+    var parsed = json(responseText);
+    if (parsed.status != 200) {
+      return
+    }
+    if (parsed.body != null && Array.isArray(parsed.body.tracks)) {
+      trackListConsumerFn(songContainer, parsed.body.tracks)
+    }
+  }
+
+  window.songTitleContainerData = {
+    container: null,
+    previouslyReceivedTracks: null
+  };
+
+  addXMLRequestCallback( function( progress ) {
+    if (!window.songTitleContainerData.container) {
+      window.songTitleContainerData.container = createHtmlElement();
+    }
+    if (typeof progress.srcElement.responseText != 'undefined' && progress.srcElement.responseText != '') {
+      parseResponse(progress.srcElement.responseText, window.songTitleContainerData.container, fillHtmlElement)
+    }
+  });
 })();
-
-
-
